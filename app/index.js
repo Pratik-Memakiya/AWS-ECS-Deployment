2d9e216714187536a75a903dd5360f9f8aec09e1
Pratik Memakiya <memakiyapratik@gmail.com>
2025-11-01 23:02:42 +0530

    destroy

--- app/index.js
+++ app/index.js
@@ -6,4 +6,4 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (_req, res) => res.send('Hello World'));
 app.get('/health', (_req, res) => res.status(200).json({ status: 'ok' }));
 
-app.listen(PORT, () => console.log(`App listening on ${PORT}`));
+app.listen(PORT,'0.0.0.0' ,() => console.log(`App listening on ${PORT}`));
