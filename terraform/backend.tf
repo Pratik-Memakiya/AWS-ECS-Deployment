b0b196e565bcb321bc3c9dd8cfe0cf802d1c1362
Pratik Memakiya <memakiyapratik@gmail.com>
2025-11-02 18:46:27 +0530

    backend state file configuration added

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "ecs-deployment-pratik"    # 🔹 Create this S3 bucket in your account
+    key            = "hello-ecs/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"             # 🔹 Optional but recommended
+    encrypt        = true
+  }
+}
