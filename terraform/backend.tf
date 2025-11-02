abc6673d2ffca30605937217559783c051376ed3
Pratik Memakiya <memakiyapratik@gmail.com>
2025-11-02 19:00:38 +0530

    Removing dynamo db

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "ecs-deployment-pratik"    # 🔹 Create this S3 bucket in your account
     key            = "hello-ecs/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"             # 🔹 Optional but recommended
+    #dynamodb_table = "terraform-lock"             # 🔹 Optional but recommended
     encrypt        = true
   }
 }
